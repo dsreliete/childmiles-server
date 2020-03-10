8b161f2f3cf58d46a9890bfd725d9438bdfd6dd1
dsreliete <dsreliete@gmail.com>
2020-03-10 17:01:51 -0400

    ädded https

--- app.js
+++ app.js
@@ -40,6 +40,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(passport.initialize());
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
 app.use('/', indexRouter);
 app.use('/child', childRouter);
 app.use('/family', familyRouter);
