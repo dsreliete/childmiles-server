a0c942f76630fe765d4c9dca4169c28fb76a7095
dsreliete <dsreliete@gmail.com>
2020-03-18 00:04:27 -0400

    update router goal: return it grouped by family id

--- models/goal.js
+++ models/goal.js
@@ -5,34 +5,38 @@ const goalSchema = new Schema({
     description: {
         type: String,
         required: true,
-        unique: true
+        unique: false
     },
     redPoint: {
         type: Number,
         min: -1,
         max: 1000,
-        required: true
+        required: true,
+        unique: false
     },
     yellowPoint: {
         type: Number,
         min: -1,
         max: 1000,
-        required: true
+        required: true,
+        unique: false
     },
     greenPoint: {
         type: Number,
         min: -1,
         max: 1000,
-        required: true
+        required: true,
+        unique: false
     },
     category:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category'
-    }
     },
-    {
-        timestamps: true
-    });
+    family: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Family'
+    }
+});
     
-module.exports = mongoose.model('Goals', goalSchema);
+module.exports = mongoose.model('Goal', goalSchema);
     
--- routes/goalRouter.js
+++ routes/goalRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Goal = require('../models/goal');
+const authentication = require('../authentication');
 
 const goalRouter = express.Router();
 
 goalRouter.use(bodyParser.json());
 
-goalRouter.route('/')
+goalRouter.route('/test')
 .get((req, res, next) => {
     Goal.find()
     .populate('category')
@@ -16,20 +17,6 @@ goalRouter.route('/')
         res.json(goals);
     })
     .catch(err => next(err));
-})
-.post((req, res, next) => {
-    Goal.create(req.body)
-    .then(goal => {
-        console.log('Goal Created ', goal);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(goal);
-    })
-    .catch(err => next(err));
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /goal');
 })
 .delete((req, res, next) => {
     Goal.deleteMany()
@@ -41,22 +28,97 @@ goalRouter.route('/')
     .catch(err => next(err));
 });
 
-goalRouter.route('/:goalId')
-.get((req, res, next) => {
-    Goal.findById(req.params.goalId)
+//________________________________________________________________//
+
+goalRouter.route('/')
+.get(authentication.verifyUser, (req, res, next) => {
+    
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to get all goals from this family"});
+        return;
+    }
+    const familyId = req.user.family;
+
+    Goal.find({family: familyId})
     .populate('category')
-    .then(goal => {
+    .then(goals => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(goal);
+        res.json(goals);
     })
     .catch(err => next(err));
+})
+.post(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
+    
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to create a new goal for this family"});
+        return;
+    }
+    const familyId = req.user.family;
+    
+    const goal = new Goal();
+    if(req.body.description){
+        goal.description = req.body.description;
+    }
+    if(req.body.greenPoint){
+        goal.greenPoint = req.body.greenPoint;
+    }
+    if(req.body.yellowPoint){
+        goal.yellowPoint = req.body.yellowPoint;
+    }
+    if(req.body.redPoint){
+        goal.redPoint = req.body.redPoint;
+    }
+    if(req.body.category){
+        goal.category = req.body.category;
+    }
+    goal.family = familyId;
+
+    Goal.create(goal)
+    .then(task => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(task);
+    })
+    .catch(err => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /goal');
+})
+.delete(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
+    
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to delete all goals from this family"});
+        return;
+    }
+    const familyId = req.user.family;
+
+    Goal.deleteMany({family: familyId})
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+goalRouter.route('/:goalId')
+.get((req, res, next) => {
+    res.statusCode = 403;
+    res.end('GET operation not supported on /goal/goalId');
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /goal/goalId');
 })
-.put((req, res, next) => {
+.put(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
     Goal.findByIdAndUpdate(req.params.goalId, {
         $set: req.body
     }, { new: true })
@@ -67,7 +129,7 @@ goalRouter.route('/:goalId')
     })
     .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
     Goal.findByIdAndDelete(req.params.goalId)
     .then(response => {
         res.statusCode = 200;
