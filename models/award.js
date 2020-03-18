a476c4b919d7aeaad9ce78a548ef15711aa56bd1
dsreliete <dsreliete@gmail.com>
2020-03-17 23:25:49 -0400

    update router award: return it grouped by family id

--- models/award.js
+++ models/award.js
@@ -5,17 +5,19 @@ const awardSchema = new Schema({
 	description: {
 		type: String,
 		required: true,
-		unique: true
+		unique: false
 	},
 	point: {
 		type: Number,
         min: 0,
         max: 1000,
-        required: true
-	}
-},
-{
-	timestamps: true
+		required: true,
+		unique: false
+	},
+	family: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Family'
+    }
 });
 
 module.exports = mongoose.model('Award', awardSchema);
--- routes/awardRouter.js
+++ routes/awardRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Award = require('../models/award');
+const authentication = require('../authentication');
 
 const awardRouter = express.Router();
 
 awardRouter.use(bodyParser.json());
 
-awardRouter.route('/')
+awardRouter.route('/test')
 .get((req, res, next) => {
     Award.find()
     .then(awards => {
@@ -15,20 +16,6 @@ awardRouter.route('/')
         res.json(awards);
     })
     .catch(err => next(err));
-})
-.post((req, res, next) => {
-    Award.create(req.body)
-    .then(award => {
-        console.log('Penalty Created ', award);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(award);
-    })
-    .catch(err => next(err));
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /award');
 })
 .delete((req, res, next) => {
     Award.deleteMany()
@@ -40,9 +27,47 @@ awardRouter.route('/')
     .catch(err => next(err));
 });
 
-awardRouter.route('/:awardId')
-.get((req, res, next) => {
-    Award.findById(req.params.awardId)
+//_____________________________________________________________//
+
+awardRouter.route('/')
+.get(authentication.verifyUser, (req, res, next) => {
+
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to get all awards from this family"});
+        return;
+    }
+    const familyId = req.user.family;
+
+    Award.find({family: familyId})
+    .then(awards => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(awards);
+    })
+    .catch(err => next(err));
+})
+.post(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
+
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to create award from this family"});
+        return;
+    }
+    const familyId = req.user.family;
+
+    const award = new Award();
+    if(req.body.description){
+        award.description = req.body.description;
+    }
+    if(req.body.point){
+        award.point = req.body.point;
+    }
+    award.family = familyId;
+
+    Award.create(award)
     .then(award => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -50,11 +75,41 @@ awardRouter.route('/:awardId')
     })
     .catch(err => next(err));
 })
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /award');
+})
+.delete(authentication.verifyUser,authentication.verifyRole, (req, res, next) => {
+
+    if(!req.user.family){
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({msg: "It is not possible to delete all awards from this family"});
+        return;
+    }
+    const familyId = req.user.family;
+
+    Award.deleteMany({family: familyId})
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+//____________________________________________________________//
+
+awardRouter.route('/:awardId')
+.get((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`GET operation not supported on /awards/${req.params.awardId}`);
+})
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /awards/${req.params.awardId}`); 
 })
-.put((req, res, next) => {
+.put(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
     Award.findByIdAndUpdate(req.params.awardId, {
         $set: req.body
     }, { new: true })
@@ -65,7 +120,7 @@ awardRouter.route('/:awardId')
     })
     .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
     Award.findByIdAndDelete(req.params.awardId)
     .then(response => {
         res.statusCode = 200;
