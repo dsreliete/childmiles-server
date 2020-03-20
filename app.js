1043120e2d5920c0b7cb9ad3710878af791a5983
dsreliete <dsreliete@gmail.com>
2020-03-20 16:27:37 -0400

    exclude comments

--- app.js
+++ app.js
@@ -31,11 +31,9 @@ connect.then(() => console.log('Connected correctly to server \o/'),
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
@@ -65,18 +63,14 @@ app.use('/realizations', realizationRouter);
 app.use('/history', historyRouter);
 app.use('/users', userRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = err;
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
