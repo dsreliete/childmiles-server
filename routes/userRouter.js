d76fc746f28cc7550a868cd831c941198067c47a
dsreliete <dsreliete@gmail.com>
2020-04-10 01:00:41 -0400

    üpdate user CRUD to return users list

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,14 +11,14 @@ const userRouter = express.Router();
 userRouter.route('/test')
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 .get(cors.cors, (req, res, next) => {
-  User.find()
-  .populate('family')
-  .then(users => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(users);
-  })
-  .catch(err => next(err));
+	User.find()
+	.populate('family')
+	.then(users => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(users);
+	})
+	.catch(err => next(err));
 })
 .post(cors.corsWithOptions, (req, res, next) => {
   res.statusCode = 403;
@@ -29,13 +29,13 @@ userRouter.route('/test')
   res.end('PUT operation not supported on /person');
 })
 .delete(cors.corsWithOptions, (req, res, next) => {
-  User.deleteMany()
-  .then(response => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(response);
-  })
-  .catch(err => next(err));
+	User.deleteMany()
+	.then(response => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(response);
+	})
+	.catch(err => next(err));
 });
 
 //______________________________________________________________________________________//
@@ -45,75 +45,106 @@ userRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 .get(cors.cors, authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
 
-  if(!req.user.family){
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({msg: "It is not possible to get all users from this family"});
-    return;
-  }
-  const familyId = req.user.family;
+	if(!req.user.family){
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({success: false, message: "It is not possible to get all users from this family"});
+		return;
+	}
+	const familyId = req.user.family;
 
-  User.find({family: familyId})
-  .populate('family')
-  .then(users => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, users: users});
-  })
-  .catch(err => next(err));
+	User.find({family: familyId})
+	.populate('family')
+	.then(users => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({success: true, users: users});
+	})
+	.catch(err => {
+		return res.status(200).json({success: false, message: "It is not possible to get all users from this family" })
+	});
 })
 .post(cors.corsWithOptions, authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
-  if(!req.user.family) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({msg: "It is not possible to get all child from this family"});
-    return;
-  }
-  const familyId = req.user.family;
+	if(!req.user.family) {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({ success:false, message: "It is not possible to create a new user for this family"});
+		return;
+	}
+	const familyId = req.user.family;
 
-  let adminUser = '';
-  User.findById(req.user._id)  
-  .then(user => {
-    adminUser = user;
-  })
-  .catch(err => {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({error: err, msg: "It is not possible to get all child from this family"});
-  })
+	let adminUser = '';
+	User.findById(req.user._id)  
+	.then(user => {
+		adminUser = user;
+	})
+	.catch(err => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({ success:false, message: "It is not possible to create a new user for this family"});
+	})
 
   //static method from passpot-local-mongoose to register username and pswd
-  User.register(new User({username: req.body.username}),
-    req.body.password,
-    (err, person) => {
-      if (err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err, msg: "It is not possible to create a new user"});
-      } else {
-        if (req.body.firstname) {
-            person.firstname = req.body.firstname;
-        }
-        if (req.body.lastname) {
-            person.lastname = req.body.lastname;
-        }
-        if (req.body.role) {
-            person.role = req.body.role;
-        }
-        if (req.body.email) {
-          person.email = req.body.email;
-        }
-        person.family = familyId;
-        person.save(err => {
-          if (err) {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({err: err, msg: "no person saved"});
-            return;
-          }
-          sendVerificationEmail(person, adminUser, req, res)
-        });
-      }
+	User.register(new User({username: req.body.username}),
+		req.body.password,
+		(err, person) => {
+		if (err) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json({success:false, message: "It is not possible to create a new user"});
+		} else {
+			if (req.body.firstname) {
+				person.firstname = req.body.firstname;
+			}
+			if (req.body.lastname) {
+				person.lastname = req.body.lastname;
+			}
+			if (req.body.role) {
+				person.role = req.body.role;
+			}
+			if (req.body.email) {
+			person.email = req.body.email;
+			}
+			person.family = familyId;
+			person.save(err => {
+				if (err) {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json({success:false, message: "It is not possible to create a new user."});
+					return;
+				}
+
+				User.find({family: familyId})
+				.then(users => {
+					const token = authentication.getEmailToken({_id: person._id});
+
+					sgMail.setApiKey(process.env.SENDGRID_KEY);
+				
+					const link="http://localhost:3000/reset-credentials/"+token;
+					const msg = {
+						to: person.email,
+						from: process.env.FROM_EMAIL,
+						subject: 'Criação de usuário do sistema Milhas Infantis',
+						text: 'Milhas Infantis: um app fácil de usar para gerenciar as atividades das crianças!',
+						html: `<p>Olá ${person.firstname},<p><br><p>O usuário admin <strong>${adminUser.firstname}</strong> te cadastrou como um novo usuário do sistema. Por favor, clique no link a seguir <a href="${link}">link</a> para criar uma nova senha.</p> 
+						<br><p>Após renovação da senha será possível fazer login ao sistema com ajuda do username <strong>${person.username}</strong> previamente cadastrado pelo usuário admin e senha recentemente cadastrada por você.</p>
+						<br><p>Caso não tenha conhecimento sobre essa situação, por favor ignore este email.</p>`
+					};
+					sgMail.send(msg)
+					.then(result => {
+						res.statusCode = 200;
+						res.setHeader('Content-Type', 'application/json');
+						res.json({success: true, users: users});	
+					})
+					.catch(err => {
+						return res.status(200).json({success: false, message: 'It was not possible to send an verification email about new user creation!'})
+					});
+				})
+				.catch(err => {
+					return res.status(200).json({success: false, message: "It is not possible to create new users for this family" })
+				});
+			});
+		}
     });
 })
 .put(cors.corsWithOptions, (req, res, next) => {
@@ -139,72 +170,70 @@ userRouter.route('/:userId')
 })
 .put(cors.corsWithOptions, authentication.verifyUser, (req, res, next) => {
 
-  if(req.params.userId != req.user._id) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({msg: "You are not allowed to do this operation!"});
-    return;
-  }
+	if(req.params.userId != req.user._id) {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({success: false, message: "You are not allowed to do this operation!"});
+		return;
+	}
 
-  User.findByIdAndUpdate(req.params.userId, {
-    $set: req.body
-  }, { new: true })
-  
-  .then(user => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(user);
-  })
-  .catch(err => next(err));
+	const familyId = req.user.family;
+
+	User.findByIdAndUpdate(req.params.userId, {
+		$set: req.body
+	}, { new: true })
+	.then(user => {
+
+		User.find({family: familyId})
+		.then(users => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json({success: true, users});
+		})
+		.catch(err => {
+			return res.status(200).json({success: false, message: "It is not possible to edit user."})
+		});
+	})
+	.catch(err => {
+		return res.status(200).json({success: false, message: "It is not possible to edit user."})
+	});
 })
 .delete(cors.corsWithOptions, authentication.verifyUser, authentication.verifyRole, (req, res, next) => {
 
-  User.findByIdAndDelete(req.params.userId)
-    .then(response => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(response); 
-    })
-    .catch(err => next(err));
+	User.findById(req.params.userId)
+	.then(user => {
+		if(user.role === "admin"){
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json({success: false, message:"It is impossible to delete admin user."});
+			return;
+		}
+
+		const familyId = req.user.family;
+
+		User.findByIdAndDelete(req.params.userId)
+		.then(response => {
+
+			User.find({family: familyId})
+			.then(users => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json({success: true, users});
+			})
+			.catch(err => {
+				return res.status(200).json({success: false, message: "It is not possible to delete user."})
+			});
+		})
+		.catch(err => {
+			return res.status(200).json({success: false, message: "It is not possible to delete users from this family" })
+		});
+
+	})
+	.catch(err => {
+		return res.status(200).json({success: false, message: "It is not possible to delete users from this family" })
+	});
 });
-
-
-//_______________________________________________________________________//
-
-userRouter.route('resetPswd/:email')
-.post(cors.corsWithOptions, () => {
-
-})
 
 //______________________________________________________________________________________________//
-
-userRouter.route('/logout')
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.get(cors.cors, authentication.verifyUser, (req, res, next) => {
-  const token = authentication.getToken({_id: req.user._id, family: req.user.family});
-  res.redirect('/');
-});
-
-function sendVerificationEmail(user, adminUser, req, res){
-  
-  const token = authentication.getEmailToken({_id: user._id});
-
-  sgMail.setApiKey(process.env.SENDGRID_KEY);
-
-  const link="https://"+req.headers.host+"/verifyEmail/"+token;
-  const msg = {
-    to: user.email,
-    from: process.env.FROM_EMAIL,
-    subject: 'Account Verification Token',
-    text: 'Child Miles: an efficient app to manage child tasks!',
-    html: `<p>Hi ${user.firstname}<p><br><p>Child Miles admin user ${adminUser.firstname} registered you as one of users of Child Miles app. Please click on the following <a href="${link}">link</a> to verify your account.</p> 
-    <br><p>If you did not request this, please ignore this email.</p>`
-  };
-  sgMail.send(msg)
-  .then(result => {
-      res.status(200).json({message: 'A verification email has been sent to ' + user.email + '.'});
-  })
-  .catch(err => console.log(err));
-}
 
 module.exports = userRouter;
